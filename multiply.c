923368337f2dbdbb819114c87ba093831512eab9
Andrew Mollohan <mollohan@bu.edu>
2016-11-16 18:58:56 -0500

    added hollow multiply.c function

---
+++ multiply.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+int main(int argc, char *argv[]){
+
+
+  // Will read argv[1] and argv[2] as Matrix A and B
+
+
+
+  // Covert Mat A/B to int arrays then take the dot product
+
+
+
+  // Write the value found to the shared memory for matmult_p.c
+
+  return 0;
+
+}
