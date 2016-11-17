16c2752fd8eab974cfe7191438fe3282c3a8e46a
Andrew Mollohan <mollohan@bu.edu>
2016-11-16 22:35:53 -0500

    mult functionality

--- multiply.c
+++ multiply.c
@@ -13,14 +13,24 @@
 
 int main(int argc, char *argv[]){
 
-
+  int sum = 0;
   // Will read argv[1] and argv[2] as Matrix A and B
+  char * matA = argv[1];
+  char * matB = argv[2];
 
-
+  //str tok both
+  char * Atok, Btok;
+  Atok = strtok(matA, " ");
+  Btok = strtok(matB, " ");
+  //tok A
+  while( (Atok!=NULL) && (Btok !=NULL) ){
+      sum += atoi(Atok)+atoi(Btok);
+      Atok = strtok(matA, " ");
+      Btok = strtok(matB, " ");
+    }
 
   // Covert Mat A/B to int arrays then take the dot product
-
-
+  fprintf(stdout, "%d\n", sum);
 
   // Write the value found to the shared memory for matmult_p.c
 
